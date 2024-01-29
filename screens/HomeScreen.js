47e13a07438049f0531f15925dc6880f8fcf7054
Ahmadjon <kamolovahmad@gmail.com>
2024-01-29 13:01:39 -0600

    homescreen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView} from "react-native";
+
+function HomeScreen(props) {
+    return (
+        <SafeAreaView>
+            <Text>Home screen</Text>
+        </SafeAreaView>
+    );
+}
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({})
